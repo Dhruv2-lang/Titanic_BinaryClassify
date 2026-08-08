66ebc0514f3e7456a710cfd4232beb4a80314149
Dhruv Singh <dhruvpratap2303@gmail.com>
2026-08-08 18:47:09 +0530

    Complete Titanic Training Baseline  Model

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 
 dataset=pd.read_csv("gender_submission.csv")
 dataset=pd.read_csv("train.csv")
@@ -37,3 +42,20 @@ Y = dataset["Survived"]
 print(X.head())
 print()
 print(Y.head())
+
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+
+model=LogisticRegression(max_iter=1000)
+model.fit(X_train,Y_train)
+
+Y_pred = model.predict(X_test)
+cm = confusion_matrix(Y_test, Y_pred)
+
+print("Confusion Matrix:")
+print(cm)
+accuracy = accuracy_score(Y_test,Y_pred)
+
+print("Accuracy:", accuracy_score(Y_test, Y_pred))
+print("Precision:", precision_score(Y_test, Y_pred))
+print("Recall:", recall_score(Y_test, Y_pred))
+print("F1 Score:", f1_score(Y_test, Y_pred))
